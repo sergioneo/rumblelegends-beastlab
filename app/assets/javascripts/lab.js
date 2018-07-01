8afa6915bc32326d3414b4b89460fe925ce418ac
Neo <sergio.yanez@alumnos.usm.cl>
2018-06-30 23:02:15 -0700

    Added color combination

--- app/assets/javascripts/lab.js
+++ app/assets/javascripts/lab.js
@@ -1,18 +1,24 @@
-var attrListDinosaurs = ["beastType", "ability", "secondAbility", "pedigree", "element", "type", "eyes", "nose", "mouth", "tail", "plume", "legs", "spikes", "wings"];
-var attrListUnicorns = ["beastType", "ability", "secondAbility", "pedigree", "element", "type", "eyes", "horn", "hair", "tail", "snout", "legs"];
+var attrListDinosaurs = ["colorCombination", "beastType", "ability", "secondAbility", "pedigree", "element", "type", "eyes", "nose", "mouth", "tail", "plume", "legs", "spikes", "wings"];
+var attrListUnicorns = ["colorCombination", "beastType", "ability", "secondAbility", "pedigree", "element", "type", "eyes", "horn", "hair", "tail", "snout", "legs"];
+var attrLengthDinosaurs = [6, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5];
+var attrLengthUnicorns = [6, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5];
 
 function calculateGeneSequence() {
 	var beastType = $(".gene-selector#beastType").val();
 	var newSeq = bigInt(0);
 	var attrList = [];
+	var attrLengths = [];
 	if (beastType == 0) {
 		attrList = attrListDinosaurs;
+		attrLengths = attrLengthDinosaurs;
 	} else if (beastType == 1) {
 		attrList = attrListUnicorns;
+		attrLengths = attrLengthUnicorns;
 	}
 	for (var i = 0; i < attrList.length; i++) {
 		console.log("Value for "+attrList[i]+" is "+$(".gene-selector#"+attrList[i]).val());
-		newSeq = newSeq.times(32).add(bigInt($(".gene-selector#"+attrList[i]).val()));
+		var len = attrLengths[i];
+		newSeq = newSeq.times(2 ** len).add(bigInt($(".gene-selector#"+attrList[i]).val()));
 	}
 	$(".final_seq").val(newSeq);
 	$("#displayImage").attr("src", "https://image-generator-beta.herokuapp.com/image/"+newSeq);
